6c29b174348ff8a121a7f439d1e0465d3f487ad9
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2024-06-21 09:55:34 -0500

    Add HintsRegistrar for resource types
    A new class called HintsRegistrar has been implemented to register certain resource types like '.pdf', '.json', '.html', '.css' and '.st'. This registrar has been imported into the HelloNativeApplication class for operational use.

--- src/main/java/dev/nathanlively/hello_native/HelloNativeApplication.java
+++ src/main/java/dev/nathanlively/hello_native/HelloNativeApplication.java
@@ -2,12 +2,14 @@ package dev.nathanlively.hello_native;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
+@ImportRuntimeHints(HintsRegistrar.class)
 @SpringBootApplication
 public class HelloNativeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloNativeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloNativeApplication.class, args);
+    }
 
 }
